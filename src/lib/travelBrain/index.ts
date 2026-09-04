388c5a02cae231d68e39b4667553615476e53ca3
intamar94 <intamar94@gmail.com>
2026-09-04 20:48:34 +0200

    Export canonical context and provider registry

--- src/lib/travelBrain/index.ts
+++ src/lib/travelBrain/index.ts
@@ -1,3 +1,6 @@
 export * from "./destinationResolver";
 export * from "./tripDeconstructor";
+export * from "./tripContext";
+export * from "./explorerIntelligence";
 export * from "./researchOrchestrator";
+export * from "./providerRegistry";
