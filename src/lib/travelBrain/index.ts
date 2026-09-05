55c4b93785cab3470424d56368cc2a1a75418d86
intamar94 <intamar94@gmail.com>
2026-09-05 22:18:09 +0200

    Export neural orchestration layer

--- src/lib/travelBrain/index.ts
+++ src/lib/travelBrain/index.ts
@@ -6,6 +6,7 @@ export * from "./researchOrchestrator";
 export * from "./reverseEngineeringOrchestrator";
 export * from "./agentContracts";
 export * from "./agentRuntime";
+export * from "./neuralOrchestrator";
 export * from "./providerRegistry";
 export * from "./capabilityRequirements";
 export * from "./capabilityAudit";
